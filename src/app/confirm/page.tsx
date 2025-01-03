283c2141f6ecff9e0bbd1498565f295090e9a9cc
Kushagra-0 <kushagrashukla.dev@gmail.com>
2025-01-03 11:59:52 +0530

    removed searchParams

--- src/app/confirm/page.tsx
+++ src/app/confirm/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation"; // Importing hooks
+import { useRouter } from "next/navigation"; // Importing hooks
 import { Suspense } from "react";
 
 export default function Confirm() {
   const router = useRouter();
-  const searchParams = useSearchParams(); // Get query params
+  // const searchParams = useSearchParams(); // Get query params
 
-  const date = searchParams.get("date");
-  const time = searchParams.get("time");
-  const guests = searchParams.get("guests");
-  const name = searchParams.get("name");
-  const contact = searchParams.get("contact");
+  // const date = searchParams.get("date");
+  // const time = searchParams.get("time");
+  // const guests = searchParams.get("guests");
+  // const name = searchParams.get("name");
+  // const contact = searchParams.get("contact");
 
   const handleGoToBookings = () => {
     router.push("/all-bookings"); // Navigate to /all-bookings
@@ -23,11 +23,11 @@ export default function Confirm() {
       <div className="w-full max-w-lg bg-white p-8 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold text-black text-center mb-6">Your Booking Confirmed</h2>
         <div className="space-y-4 text-gray-800">
-          <p><strong>Date:</strong> {date}</p>
+          {/* <p><strong>Date:</strong> {date}</p>
           <p><strong>Time:</strong> {time}</p>
           <p><strong>Guests:</strong> {guests}</p>
           <p><strong>Name:</strong> {name}</p>
-          <p><strong>Contact:</strong> {contact}</p>
+          <p><strong>Contact:</strong> {contact}</p> */}
         </div>
         <div className="mt-6">
           <button
